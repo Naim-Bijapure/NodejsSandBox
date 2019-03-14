169c37a7bcd1fa06d789250038287d60d892a8cd
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-14 20:09:43 +0530

    updating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import { GraphQLServer } from "graphql-yoga";
-import { Query } from "./Query";
-const resolvers={
-    Query
-}
-const server = new GraphQLServer({
-    typeDefs:`${__dirname}/schema.graphql`,
-    resolvers
-})
-server.start(()=>{
-    console.log('server is running at 4000');
-    
-})
+import express from 'express'
+import expressGraphql from 'express-graphql'
+import graphql,{buildSchema} from 'graphql'
+
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String,
+        name:String
+    }
+`);
+
+// Root resolver
+var root = {
+    message: 'Hello World!',
+    name:'naim'
+};
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql', expressGraphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
