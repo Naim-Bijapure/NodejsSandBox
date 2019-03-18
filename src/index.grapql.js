add0cf0dabe8edc8867bead294a309d7e6407e2d
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-18 08:13:30 +0530

    learned basics of  graphql now moving towords the apollo server

---
+++ src/index.grapql.js
@@ -0,0 +1,75 @@
+import express from 'express'
+import expressGraphql from 'express-graphql'
+import{graphql, buildSchema} from 'graphql'
+
+
+// GraphQL schema
+var schema = buildSchema(`
+input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+
+`);
+
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Maps username to content
+var fakeDatabase = {};
+var root = {
+ getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    // Create a random id for our "database".
+    var id = 10;
+     
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+
+};
+
+const app = express();
+app.use('/graphql',expressGraphql({
+    schema:schema,
+    rootValue:root,
+    graphiql:true
+}));
+
+app.listen(4000,()=>console.log('server started'));
--- src/index.js
+++ src/index.js
@@ -1,39 +1,34 @@
-import express from 'express'
-import expressGraphql from 'express-graphql'
-import{graphql, buildSchema} from 'graphql'
+import {ApolloServer,gql} from 'apollo-server'
+const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ];
+
+  const typeDefs=gql`
+    type Book{
+        title:String,
+        author:String
+    }
+    type Query{
+        books:[Book]
+    }
+  `
+
+  const resolvers={
+      Query:{
+          books:()=>books
+      }
+  }
+
+  const server = new ApolloServer({typeDefs,resolvers});
 
 
-// GraphQL schema
-var schema = buildSchema(`
-   type Person{
-       id:Int,
-       name:String
-   }
-  type Query{
-      N(data:String):String,
-      person:Person,
-      persons(id:Int):[Person]
-  } 
-
-`);
-
-function myOperation(data) {
-    console.log('data is ',data);
-    
-    return "cool performing operation "  
-}
-const persons=[{ id:1,name:'Pronoob'}, {id:3,name:'Asma'}, {id:2,name:'Naim'}]
-var root = {
-    N:(data)=>{return "yo got the data"+data.data} ,
-    person:()=>{ return {id:1,name:'Pronoob'}},
-    persons:(p)=>{return persons.filter((pp)=>pp.id==p.id)}
-};
-
-const app = express();
-app.use('/graphql',expressGraphql({
-    schema:schema,
-    rootValue:root,
-    graphiql:true
-}));
-
-app.listen(4000,()=>console.log('server started'));
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
