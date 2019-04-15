815230050772ce941fabc9f77c0bc418ad43d4a4
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-04-15 09:47:06 +0530

    updating

--- src/index.js
+++ src/index.js
@@ -1,74 +1,40 @@
-// Create an array of objects which hold the denominations and their values
-var denom = [
-    { name: 'ONE HUNDRED', val: 100.00},
-    { name: 'TWENTY', val: 20.00},
-    { name: 'TEN', val: 10.00},
-    { name: 'FIVE', val: 5.00},
-    { name: 'ONE', val: 1.00},
-    { name: 'QUARTER', val: 0.25},
-    { name: 'DIME', val: 0.10},
-    { name: 'NICKEL', val: 0.05},
-    { name: 'PENNY', val: 0.01}
-  ];
+const path = require('path')
+const eventEmitter = require('events');
+const os = require('os');
+
+const em = new eventEmitter();
+
+console.log('hello naim');
+process.nextTick(run1);
+
+function run1() {
+  console.log('cool running 1'); 
   
-  function checkCashRegister(price, cash, cid) {
-    var output = { status: null, change: [] };
-    var change = cash - price;
+}
+function run2() {
+  console.log('cool running 2'); 
   
-    // Transform CID array into drawer object
-    var register = cid.reduce(function(acc, curr) {
-      acc.total += curr[1];
-      acc[curr[0]] = curr[1];
-      return acc;
-    }, { total: 0 });
+}
+function run3() {
+  console.log('cool running 3 '); 
   
-    // Handle exact change
-    if (register.total === change) {
-      output.status = 'CLOSED';
-      output.change = cid;
-      return output;
-    }
-  
-    // Handle obvious insufficient funds
-    if (register.total < change) {
-      output.status = 'INSUFFICIENT_FUNDS';
-      return output;
-    }
-  
-    // Loop through the denomination array
-    var change_arr = denom.reduce(function(acc, curr) {
-      var value = 0;
-      // While there is still money of this type in the drawer
-      // And while the denomination is larger than the change remaining
-      while (register[curr.name] > 0 && change >= curr.val) {
-        change -= curr.val;
-        register[curr.name] -= curr.val;
-        value += curr.val; 
-        
-  
-        // Round change to the nearest hundreth deals with precision errors
-        change = Math.round(change * 100) / 100;
-      }
-      // Add this denomination to the output only if any was used.
-      if (value > 0) {
-          acc.push([ curr.name, value ]);
-      }
-      return acc; // Return the current change_arr
-    }, []); // Initial value of empty array for reduce
-  
-    // If there are no elements in change_arr or we have leftover change, return
-    // the string "Insufficient Funds"
-    if (change_arr.length < 1 || change > 0) {
-      output.status = 'INSUFFICIENT_FUNDS';
-      return output;
-    }
-  
-    // Here is your change, ma'am.
-    output.status = 'OPEN';
-    output.change = change_arr;
-    return output; /*?*/
-    
-  }
-  
-  // test here
-  checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+}
+
+setTimeout(run2, 0);
+setTimeout(run3, 0);
+
+process.on('exit',()=>{
+  console.log('bye bye',process.cwd());
+  process.stdout.write('cool man'); 
+
+});
+
+
+
+em.on('start',function (a,b) {
+  // console.log(a, b, this, this === em);
+console.log('started uptime is ',os.uptime)
+})
+
+em.emit('start','a','b')
+em.emit('start','a','b')
