34c9f9208fd1457d469a8184875cad141a30449a
NaimBijapure <naim@vinnovtetechnologies.com>
2019-09-05 10:11:35 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import path from 'path'
+import mysql from 'mysql'
 
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1004',
+  database : 'vplc'
+});
+
+connection.connect();
+
+
+ for (let i = 7; i < 10; i++)  {
+ for (let k = 1; k < 241; k++)  {
+           connection.query('insert into vshift_register_target_value(REGISTER_ID,SHIFT_ID,TARGET_VALUE) values(?,?,?)',[k,i,0], function (error, results, fields) {
+       if (error) throw error;
+       console.log('The result is: ', results);
+      });
+    
+  }
+}
+
+
+connection.end();
