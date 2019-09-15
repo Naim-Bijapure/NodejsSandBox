38db37a4b983aef84383b4271a2da68f7a8afb0d
NaimBijapure <naim@vinnovtetechnologies.com>
2019-09-15 22:40:39 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// import express from 'express'
+
 import mysql from 'mysql'
 
 
@@ -11,15 +13,22 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
- for (let i = 7; i < 10; i++)  {
+for (let i = 1; i < 11; i++)  {
  for (let k = 1; k < 241; k++)  {
-           connection.query('insert into vshift_register_target_value(REGISTER_ID,SHIFT_ID,TARGET_VALUE) values(?,?,?)',[k,i,0], function (error, results, fields) {
+       
+        connection.query('insert into vshift_register_target_value(REGISTER_ID,SHIFT_ID,TARGET_VALUE) values(?,?,?)',[k,i,0], function (error, results, fields) {
        if (error) throw error;
        console.log('The result is: ', results);
       });
-    
+
   }
 }
 
 
-connection.end();
+// connection.end();
+
+// const app = express();
+// app.get('/N', function (req, res) {
+//   return res.send('hello nalin=');
+// });
+// app.listen(3000, () => { console.log('listining on 3000') });
